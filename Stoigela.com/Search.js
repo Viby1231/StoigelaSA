2ad3f7cc2781ae90b95a00cad34d27b1fc502d20
Viby1231 <90258090+Viby1231@users.noreply.github.com>
2024-11-04 15:21:29 +0200

    Working search

--- Stoigela.com/Search.js
+++ Stoigela.com/Search.js
@@ -1,25 +1,25 @@
 
 // Array of content on your website
 const websiteContent = [
-  { title: "Home", url: "/", description: "Welcome to the homepage of our website." },
+  { title: "Home", url: "./", description: "Welcome to the homepage of our website." },
   { title: "Home: How can minerals keep us healthy?", url: "/#Home1", description: "" },
   { title: "Home: Are all mineral supplements created equal?", url: "/#Home2", description: "" },
 
-  { title: "About Us", url: "StoigelaSA/Stoigela.com/Pages/AboutUs.html", description: "Learn more about our team and mission." },
-  { title: "About Us: Our goal", url: "/Stoigela.com/Pages/AboutUs.html#About1", description: "Learn more about our team's goal." },
+  { title: "About Us", url: "./Stoigela.com/Pages/AboutUs.html", description: "Learn more about our team and mission." },
+  { title: "About Us: Our goal", url: "./Stoigela.com/Pages/AboutUs.html#About1", description: "Learn more about our team's goal." },
 
-  { title: "Products", url: "/Stoigela.com/Pages/Products.html", description: "Read about our Products." },
+  { title: "Products", url: "./Stoigela.com/Pages/Products.html", description: "Read about our Products." },
 
-  { title: "Contact", url: "/Stoigela.com/Pages/Contact.html", description: "Get in touch with us via email." },
+  { title: "Contact", url: "./Stoigela.com/Pages/Contact.html", description: "Get in touch with us via email." },
 
-  { title: "Research", url: "/Stoigela.com/Pages/Research.html", description: "" },
+  { title: "Research", url: "./Stoigela.com/Pages/Research.html", description: "" },
 
 
-  { title: "Topical", url: "/Stoigela.com/Pages/Topical.html", description: "Find out more about Zoilevium Topical" },
-  { title: "Immune Support", url: "/Stoigela.com/Pages/Immune Support.html", description: "Find out more about Zoilevium Immune Support" },
-  { title: "PureGut", url: "/Stoigela.com/Pages/PureGut.html", description: "Find out more about Zoilevium PureGut" },
-  { title: "References", url: "/Stoigela.com/Pages/References.html", description: "" },
-  { title: "Testimonials", url: "/Stoigela.com/Pages/TestimonialsTopical.html", description: "" }
+  { title: "Topical", url: "./Stoigela.com/Pages/Topical.html", description: "Find out more about Zoilevium Topical" },
+  { title: "Immune Support", url: "./Stoigela.com/Pages/Immune Support.html", description: "Find out more about Zoilevium Immune Support" },
+  { title: "PureGut", url: "./Stoigela.com/Pages/PureGut.html", description: "Find out more about Zoilevium PureGut" },
+  { title: "References", url: "./Stoigela.com/Pages/References.html", description: "" },
+  { title: "Testimonials", url: "./Stoigela.com/Pages/TestimonialsTopical.html", description: "" }
 ];
 
 function searchWebsite() {
